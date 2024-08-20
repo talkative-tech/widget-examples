76438ac2e22b4d3c94c43d0ca00dfc3d5bc2b352
Andrew Arscott <andrew@prometheuscomputing.co.uk>
2024-08-20 15:06:14 +0100

    Updates auto start example

--- chat-with-autostart/tailwind.config.js
+++ chat-with-autostart/tailwind.config.js
@@ -1,12 +1,16 @@
 // tailwind.config.js
 module.exports = {
-  mode: "jit", // enable tailwind just in time
-  purge: ["./index.html"],
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	mode: "jit", // enable tailwind just in time
+	purge: ["./index.html"],
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [
+		require("@tailwindcss/forms"),
+		require("@tailwindcss/aspect-ratio"),
+
+	],
 };
